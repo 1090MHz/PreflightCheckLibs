1896b641dbd65f10065a3bb6f6c7087e1f475069
1090MHz <107646764+1090MHz@users.noreply.github.com>
2024-08-14 12:42:34 -0600

    refactor and add fonts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# copy files to the export directory
-cp -r /app/export/glfw /mnt/export
-cp -r /app/export/glew /mnt/export
-cp -r /app/export/lib/imgui/emscripten /mnt/export
+# Ensure the target directories exist
+mkdir -p /mnt/export/libs
+mkdir -p /mnt/export/fonts
 
-cp -r /app/export/lib/freetype/ /mnt/export
+# copy files to the export directory
+cp -r /app/export/glfw/ /mnt/export/libs
+cp -r /app/export/glew/ /mnt/export/libs
+cp -r /app/export/lib/imgui/emscripten/ /mnt/export/libs
+cp -r /app/export/lib/freetype/ /mnt/export/libs
+
+# Copy fonts to the export directory
+cp -r /app/export/fonts /mnt/export
 
 # Add any other commands you need to run here
 
